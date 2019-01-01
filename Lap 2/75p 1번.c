b01b65ab2ce7b0e2c5299cef88e96049d203539b
Ha-moon <43697574+Ha-moon@users.noreply.github.com>
2019-01-01 20:48:16 +0900

    Add files via upload

---
+++ Lap 2/75p 1번.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("이름:");
+	scanf_s("%d", &name);
+
+	print("주소:");
+	scanf_s("%d", &address);
+
+	printf("전화번호:");
+	scanf_s("%d", &number);
+
+	return 0;
+}
